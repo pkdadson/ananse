36b0f4981353a1c1f701222a82efdeee7c17e2be
Dadson <pddadson@gmail.com>
2026-07-28 20:39:01 +0200

    feat(tokens,react): scaffold @canvas/tokens and @canvas/react packages

---
+++ packages/react/tests/setup.ts
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/vitest';
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+globalThis.ResizeObserver = ResizeObserverMock as unknown as typeof ResizeObserver;
+
+class DOMMatrixReadOnlyMock {
+  m22 = 1;
+  constructor() {}
+}
+globalThis.DOMMatrixReadOnly = DOMMatrixReadOnlyMock as unknown as typeof DOMMatrixReadOnly;
